40e74583bde2966443a708184e6f06be4c1ddca9
CocoaBuns <62471279+CocoaBuns@users.noreply.github.com>
2020-04-08 15:08:01 +0800

    Update 6

--- src/com/company/MainApp.java
+++ src/com/company/MainApp.java
@@ -10,5 +10,6 @@ public class MainApp
 		System.out.println("Update Test 3.");
 		System.out.println("Update Test 4.");
 		System.out.println("Update Test 5.");
+		System.out.println("Update Test 6.");
 	}
 }
